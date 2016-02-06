dc7b1261f51b62074ed2348dc601030e7566939a
Daniel García <danigcaff@gmail.com>
2016-02-06 14:05:15 +0100

    Initial commit (basic maven structure)

---
+++ spring-samples-oauth/src/main/java/com/danigcaff/springframework/samples/spring_samples_oauth/Application.java
@@ -0,0 +1,13 @@
+package com.danigcaff.springframework.samples.spring_samples_oauth;
+
+/**
+ * Hello world!
+ *
+ */
+public class Application 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
