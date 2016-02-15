4fc7cb9ff8fe13de828b7f1e89dadc0a8274dd69
Daniel García <danigcaff@gmail.com>
2016-02-15 17:06:18 +0100

    Habilito /event para punto de entrada de github

--- spring-samples-oauth/src/main/java/com/danigcaff/springframework/samples/spring_samples_oauth/MainController.java
+++ spring-samples-oauth/src/main/java/com/danigcaff/springframework/samples/spring_samples_oauth/MainController.java
@@ -1,8 +1,9 @@
 package com.danigcaff.springframework.samples.spring_samples_oauth;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -12,5 +13,12 @@ public class MainController
     @ResponseBody
     public String index() {
     	return "Hola mundo!!";
+    }
+    
+    @RequestMapping(value = "/event", method = RequestMethod.POST)
+    @ResponseBody
+    public String eventListener(@RequestBody String evento) {
+    	System.out.println(evento);
+    	return "OK";
     }
 }
