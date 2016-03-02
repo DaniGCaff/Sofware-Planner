2cb5a7a0d94d3c79471bbc25c2627d189f74e675
Rober <danigcaff@gmail.com>
2016-03-02 18:57:51 +0100

    con fallo

--- spring-samples-oauth/src/main/java/com/danigcaff/springframework/samples/spring_samples_oauth/MainController.java
+++ spring-samples-oauth/src/main/java/com/danigcaff/springframework/samples/spring_samples_oauth/MainController.java
@@ -6,19 +6,42 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.util.JSON;
+
 @Controller
-public class MainController 
+public class MainController
 {
+    private MongoCollection coll;
+    private MongoDatabase database;
+    private MongoClient client;
     @RequestMapping("/")
     @ResponseBody
     public String index() {
     	return "Hola mundo!!";
     }
     
+    public void mongoDBConnection(){
+    	client = new MongoClient("localhost", 27017);
+    	database=client.getDatabase("trellogithub");
+    	coll=database.getCollection("eventos");
+    }
+    
     @RequestMapping(value = "/event", method = RequestMethod.POST)
     @ResponseBody
     public String eventListener(@RequestBody String evento) {
     	System.out.println(evento);
+    	mongoDBConnection();
+    	 DBObject doc = (DBObject) JSON.parse(evento);
+    	 
+         coll.insertOne(doc);
     	return "OK";
     }
+    
 }
