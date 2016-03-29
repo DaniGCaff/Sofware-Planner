8cbce17d7cddb14f3686c0f207d451b090fc5a1c
Daniel García <danigcaff@gmail.com>
2016-03-29 18:17:31 +0200

    Anotaciones que faltaban en UserController

--- spring-web/src/main/java/com/danigcaff/springframework/samples/spring_web/UserController.java
+++ spring-web/src/main/java/com/danigcaff/springframework/samples/spring_web/UserController.java
@@ -4,10 +4,12 @@ import java.security.Principal;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableAuthorizationServer
 public class UserController {
 	@RequestMapping({ "/user", "/me" })
 	public Map<String, String> user(Principal principal) {
